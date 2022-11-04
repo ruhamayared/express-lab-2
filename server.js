848469d9519f5195cb12fc1057e55a3d66b079dc
Ruhama Yared <ruhamayared1@gmail.com>
2022-11-03 22:22:07 -0400

    fifth commit

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.get('/tip/:total/:tipPercentage', (request, response) => {
 //Magic 8 Ball
 const randomResponse = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
-const randomItem = randomResponse[Math.floor(Math.random()*randomResponse.length)]
 
 app.get('/magic/:phrase/', (request, response) => {
-    response.send(`${request.params.phrase}` + `<h1>${randomItem}</h1>`)
+    const randomItem = randomResponse[Math.floor(Math.random()*randomResponse.length)]
+    response.send(`${request.params.phrase} <h1>${randomItem}</h1>`)
 })
 
 
